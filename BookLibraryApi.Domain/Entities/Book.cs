30ce8c13248e2da55fb13ecbd4dfacbaac92a1d6
DevDaniloFerrari <consultor.ferrari@leomadeiras.com.br>
2023-03-06 22:57:45 -0300

    new properties on book

--- BookLibraryApi.Domain/Entities/Book.cs
+++ BookLibraryApi.Domain/Entities/Book.cs
@@ -7,5 +7,12 @@ namespace BookLibraryApi.Domain.Entities
         [Key]
         public int Id { get; set; }
         public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int TotalCopies { get; set; }
+        public int CopiesInUse { get; set; }
+        public string Type { get; set; }
+        public string Isbn { get; set; }
+        public string Category { get; set; }
     }
 }
---
+++ BookLibraryApi.Infra/Migrations/20230307015321_AddPropertiesOnBookEntity.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookLibraryApi.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertiesOnBookEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CopiesInUse",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Isbn",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalCopies",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CopiesInUse",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Isbn",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TotalCopies",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Books");
+        }
+    }
+}
