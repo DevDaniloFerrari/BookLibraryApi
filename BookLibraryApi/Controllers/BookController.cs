137eab2ac945f5e2d17d98030249c736db950909
DevDaniloFerrari <consultor.ferrari@leomadeiras.com.br>
2023-03-09 08:15:03 -0300

    delay on controller

--- BookLibraryApi/Controllers/BookController.cs
+++ BookLibraryApi/Controllers/BookController.cs
@@ -7,17 +7,22 @@ namespace BookLibraryApi.Controllers
     [Route("books")]
     public class BookController : Controller
     {
-
         [HttpGet]
-        public IActionResult GetBooks([FromServices] IGetBooksService service)
+        public async Task<IActionResult> GetBooks([FromServices] IGetBooksService service)
         {
+            //Only to be able to show spinner on frontend
+            await Task.Delay(1000);
+
             var result = service.GetBooks();
             return Ok(result);
         }
 
         [HttpGet("{property}/{value}")]
-        public IActionResult GetBooks([FromServices] IGetBooksService service, [FromRoute] string property, [FromRoute] string value)
+        public async Task<IActionResult> GetBooks([FromServices] IGetBooksService service, [FromRoute] string property, [FromRoute] string value)
         {
+            //Only to be able to show spinner on frontend
+            await Task.Delay(1000);
+
             var result = service.GetBooks(property, value);
             return Ok(result);
         }
